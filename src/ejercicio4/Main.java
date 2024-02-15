39b3559f17c9318dd27e0bfa11008dc264f8f4a7
dpadilla22 <160169459+dpadilla22@users.noreply.github.com>
2024-02-15 12:44:10 -0700

    ejercicio 4

---
+++ src/ejercicio4/Main.java
@@ -0,0 +1,16 @@
+package ejercicio4;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		JFrame Ventana= new Ventana();
+		Ventana.setTitle("mi primer ventanita :p");
+		Ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Ventana.setVisible(true);
+	}
+
+}
---
+++ src/ejercicio4/Ventana.java
@@ -0,0 +1,11 @@
+/*package ejercicio4;
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
+
+	public Ventana() {
+		setSize (500,500);
+		setLocationRelativeTo(null);
+	}
+}
